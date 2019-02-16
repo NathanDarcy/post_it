09c72fd318097cdc5b711e33260d5e67b49b423d
nathandarcy33 <nathandarcy33@gmail.com>
2019-02-16 11:09:38 -0600

    Added necessary structure for BCrypt password encoding from Spring Security Created test method to verify that hashes are encrypted and retrieved properly

--- src/main/java/com/nathan/post_it/Service/UserService.java
+++ src/main/java/com/nathan/post_it/Service/UserService.java
@@ -3,6 +3,7 @@ package com.nathan.post_it.Service;
 import com.nathan.post_it.Entity.User;
 import com.nathan.post_it.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,7 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public User findUserByUsernameAndPassword(String username, String password){
-
-        return userRepository.findUserByUsernameAndPassword(username, password);
+    public User findUserByUsername(String username){
+        return userRepository.findUserByUsername(username);
     }
 }
--- src/test/java/com/nathan/post_it/PostItApplicationTests.java
+++ src/test/java/com/nathan/post_it/PostItApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,15 +16,36 @@ public class PostItApplicationTests {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Test
     public void contextLoads() {
     }
 
     @Test
-    public void testUserDB() {
-        User testUser = userService.findUserByUsernameAndPassword("testUsername", "testPassword");
+    public void retrieveUser(){
+        User testUser = userService.findUserByUsername("testUsername");
 
-        System.out.println("User: " + testUser.getUsername() + ": " + testUser.getPassword());
+        // Check if user entered password ("testPassword") matches hash stored in DB
+        if (bCryptPasswordEncoder.matches("testPassword", testUser.getPassword())){
+            System.out.println("Username/Password pair found");
+        }
+        else {
+            System.out.println("Password did not match");
+        }
+    }
+
+    @Test
+    public void testBCrypt(){
+
+        String salt = "testSalt";
+
+        String plain = "testPassword";
+        String encoded = bCryptPasswordEncoder.encode(plain); //$2a$10$nCdnBnsHuIo7rrkXBdqdeOKClCdJFSwcREbyDbZc.s7P2mP/puLAO
+
+        System.out.println("input : hash =" + plain + " : " + encoded);
+
     }
 
 }
