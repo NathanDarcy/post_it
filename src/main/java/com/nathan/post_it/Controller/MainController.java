437b71c912e84c02c8c93dbec350e78670aac178
nathandarcy33 <nathandarcy33@gmail.com>
2019-02-17 11:32:58 -0600

    Added list page which displays all discussion from db

--- src/main/java/com/nathan/post_it/Controller/MainController.java
+++ src/main/java/com/nathan/post_it/Controller/MainController.java
@@ -1,20 +1,35 @@
 package com.nathan.post_it.Controller;
 
+import com.nathan.post_it.Entity.Discussion;
 import com.nathan.post_it.Entity.User;
 import com.nathan.post_it.Repository.UserRepository;
+import com.nathan.post_it.Service.DiscussionService;
 import com.nathan.post_it.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private DiscussionService discussionService;
 
-    @RequestMapping("/")
-    public String test(){
-        return "index";
+    @RequestMapping({"/", "", "/home"})
+    public ModelAndView home(){
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping("/list")
+    public ModelAndView list(Model model){
+        Iterable<Discussion> discussions = discussionService.findall();
+
+
+        model.addAttribute("discussions", discussions);
+        return new ModelAndView("list");
     }
 }
